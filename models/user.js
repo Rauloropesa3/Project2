511e0328fb3dd689eb3506203f68673c4e86b505
erik G <egomezfax19@gmail.com>
2020-10-10 11:04:29 -0700

    updated user.js

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-    pastDiagnosis:{
+    pastDiagnosis: {
       type: DataTypes.STRING,
       allowNull: true
     }
